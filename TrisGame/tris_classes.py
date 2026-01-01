fdc843afea8baa78f3940334142de42c8d3acba7
Andrea Rossi <andrea33.rossi@mail.polimi.it>
2026-01-01 23:53:49 +0100

    fix to corner trap

--- TrisGame/tris_classes.py
+++ TrisGame/tris_classes.py
@@ -493,6 +493,18 @@ class TrisCPU():
                 pos_to_iter = unfilled_slots - set(corner_positions)
                 return list(pos_to_iter)[0]
             else:
+                cross_positions = [(0,1), (1,2), (2,1), (1,0)]
+                for i in range(len(cross_positions)):
+                    cross_pos1 = cross_positions[reset_index_to_range(i, len(cross_positions))]
+                    cross_pos2 = cross_positions[reset_index_to_range(i+1, len(cross_positions))]
+
+                    if self.grid.grid_values[cross_pos1[0], cross_pos1[1]] == 1 and self.grid.grid_values[cross_pos2[0], cross_pos2[1]] == 1:
+                        corner_from_cross = get_corner_from_neighbouring_slots(cross_pos1, cross_pos2)
+                        if self.grid.grid_values[corner_from_cross[0], corner_from_cross[1]] == 0:
+                            return corner_from_cross
+                    else:
+                        continue
+
                 for corner in corner_positions:
                     if self.grid.grid_values[corner[0], corner[1]] == 0:
                         return corner
@@ -523,3 +535,27 @@ def get_opposite_index(idx, len):
         return mid_idx - (idx-mid_idx)
     else:
         return mid_idx
+
+def reset_index_to_range(idx, len):
+    if idx <= len - 1:
+        return idx
+    else:
+        return idx - len
+
+def get_corner_from_neighbouring_slots(slot1, slot2):
+    y1, y2 = slot1[1], slot2[1]
+    x1, x2 = slot1[0], slot2[0]
+    x_corner = None
+    y_corner = None
+
+    if min(x1, x2) == 0:
+        x_corner = 0
+    elif max(x1, x2) == 2:
+        x_corner = 2
+
+    if min(y1, y2) == 0:
+        y_corner = 0
+    elif max(y1, y2) == 2:
+        y_corner = 2
+
+    return (x_corner, y_corner)
